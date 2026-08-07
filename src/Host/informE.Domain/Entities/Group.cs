a50ce8d563e76662b13cfb685c238df3c183625f
guto401 <87216391+guto401@users.noreply.github.com>
2026-08-07 19:40:50 -0300

    fix(Group.cs)
    O que mudou:
    + Tirei as chaves dos ifs;
    + Organizei os métodos em métodos de domínio e métodos de validação (só por fins de organização do código e boa leitura);
    + Arrumei o método pra limitar o tamanho dos caracteres na validação da description;

--- src/Host/informE.Domain/Entities/Group.cs
+++ src/Host/informE.Domain/Entities/Group.cs
@@ -19,9 +19,7 @@ public class Group
     public Group(string name, string? description, Guid ownerId)
     {
         if (ValidateName(name))
-        {
             Name = name;
-        }
 
         Description = description;
         IsActive = true;
@@ -29,8 +27,7 @@ public class Group
         OwnerId = ownerId;
     }
 
-    //Criação dos métodos
-
+    //Métodos de validação
     public static bool ValidateName(string name)
     {
         if (string.IsNullOrEmpty(name))
@@ -42,29 +39,24 @@ public class Group
         return true;
     }
 
+    public static bool ValidateDescription(string? description)
+    {
+        if(description != null && description.Length > 100)
+            throw new ArgumentException("A descrição ultrapassou o limite de caracteres.");
+
+        return true;
+    }
+
+    // Métodos de domínio
     public void UpdateGroupName(string name)
     {
         if (ValidateName(name))
-        {
             Name = name;
-        }
-        
-    }
-
-    public static bool ValidateDescription(string? description)
-    {
-        if(description != null && description > 100)
-            throw new ArgumentException("A descrição ultrapassou o limite de caracteres.");
-
-        return true
     }
 
     public void UpdateDescription(string? description)
     {
         if (ValidateDescription(description))
-        {
             Description = description;
-        }
     }
-
 }
